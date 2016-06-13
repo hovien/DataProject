e6d8f4c250dd5baa3f5ea771f286b77497e7d304
hovien <a1500567@myy.haaga-helia.fi>
2016-06-13 14:14:27 +0300

    Update preproc.py

--- preproc.py
+++ preproc.py
@@ -4,17 +4,26 @@ import re
 
 txtinput = open(raw_input("input file name: "),)
 stop_input = open(raw_input("stop word list: "))
+abbr_input = open(raw_input("abbreviation list: "))
 txtoutput = open(raw_input("output file name: "), "w")
 
-# reading the file, splitting it into sentences, saving into a list
+#creating the stopword and abbreviation list
+stop_list = (stop_input.read()).split("\n")
+abbr_list = (abbr_input.read()).lower().split("\n")
+
+# reading the file, geting rid of abbreviations, splitting it into sentences, saving into a list
 sentence_list = []
 for line in txtinput:
-    output = line.rstrip().lower()
+    abbr_check = line.rstrip().lower().split(" ")
+    output = ""
+    for word in abbr_check:
+        if word in abbr_list:
+            print word            
+            del word
+            continue
+        output += word + " "
     output = output.split("." or "?" or "!")   
     sentence_list.extend(output)
-
-#creating the stopword list
-stop_list = (stop_input.read()).split("\n")
 
 #removing stop words and non-alphabeting characters
 output_string = ""
@@ -31,4 +40,4 @@ for sentence in sentence_list:
     output_string += output_sentence + "\n"
 
 #decoding-encoding and writing the result in the output
-txtoutput.write(output_string)
+txtoutput.write(output_string.decode("windows 1252").encode("utf-8"))
